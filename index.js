b641ceac0ec3cb9013845a12d6aba31fb268936a
Taqtile <taqtile@Demoiselle.local>
2022-01-07 13:51:10 -0300

    Navigating  to a blank page

--- index.js
+++ index.js
@@ -4,13 +4,11 @@
 import React from 'react';
 import { Navigation } from 'react-native-navigation';
 import App from './App';
-import { SettingsScreen } from './SettingsScreen';
 
 const HomeScreen = (props) => {
   return <App componentId={props.componentId} />;
 };
 Navigation.registerComponent('Home', () => HomeScreen);
-Navigation.registerComponent('Settings', () => SettingsScreen);
 Navigation.events().registerAppLaunchedListener(async () => {
   Navigation.setRoot({
     root: {
