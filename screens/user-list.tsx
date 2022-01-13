a7b4a656a1a8cd21f793317261e83401e3e31231
Taqtile <taqtile@Demoiselle.local>
2022-01-13 10:13:02 -0300

    rebase with develop

--- screens/user-list.tsx
+++ screens/user-list.tsx
@@ -1,21 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
 import React, { useState } from 'react';
 import { View, Text, FlatList, StyleSheet, ActivityIndicator } from 'react-native';
-import { NetworkStatus, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { getUsers } from '../components/utils/graphql-requests';
 
 const UserListScreen = () => {
   const [offset, setOffset] = useState(0);
 
-  const { loading, error, data, refetch, networkStatus } = useQuery(getUsers, {
+  const { loading, error, data, refetch } = useQuery(getUsers, {
     variables: { offset: offset },
     notifyOnNetworkStatusChange: true,
   });
@@ -34,21 +25,25 @@ const UserListScreen = () => {
     }
   };
 
+  const renderItem = (item: any) => {
+    return (
+      <>
+        <Text style={styles.item}>{`Nome : ${item.name}`}</Text>
+        <Text style={styles.item}>{`Email : ${item.email}`}</Text>
+      </>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
         data={users}
         onEndReached={UpdateOffset}
         keyExtractor={(item, index) => String(index)}
-        renderItem={({ item }) => (
-          <>
-            <Text style={styles.item}>{`Nome : ${item.name}`}</Text>
-            <Text style={styles.item}>{`Email : ${item.email}`}</Text>
-          </>
-        )}
+        renderItem={renderItem}
       />
       {error ?? <Text>{error}</Text>}
-      {(loading || networkStatus === NetworkStatus.refetch) && <ActivityIndicator color='black' />}
+      {loading && <ActivityIndicator color='black' />}
     </View>
   );
 };
